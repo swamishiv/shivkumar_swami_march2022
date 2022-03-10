9b5f56a11753c8fec9372117321034a23e0d9a64
swamishiv <100920263+swamishiv@users.noreply.github.com>
2022-03-10 23:24:02 +0530

    Add files via upload

---
+++ T12.java
@@ -0,0 +1,17 @@
+
+class T12
+{
+	 public static void main (String args[])
+	{ 
+	 double a,b,c,d,e,f,g,h;
+	 a= 1.0/3;
+	 b= 1.0/5;
+	 c=1.0/7;
+	 d=1.0/9;
+	 e=1.0/11;
+	 f=a+b-c+d-e;
+	 g=1-f;
+	 h=4.0*g;
+	 System.out.println( h );
+	  }
+}
